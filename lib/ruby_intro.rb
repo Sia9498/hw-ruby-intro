e92340ac44869fa0bf7c752118de65816b031cff
EC2 Default User <ec2-user@ip-172-31-52-12.ec2.internal>
2021-09-16 02:42:47 +0000

    Final

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -80,5 +80,16 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+attr_accessor :isbn,  :price
+
+	def initialize isbn, price
+		raise ArgumentError if isbn.empty? || price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		return "$%.2f" % price
+	end
 end
